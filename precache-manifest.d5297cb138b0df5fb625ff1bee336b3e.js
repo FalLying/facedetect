7aea0672e0fa33347320c81f156ee940a8526bc7
Jonas Brito <jonasexplore@gmail.com>
2020-05-29 15:59:04 -0300

    Updates

---
+++ precache-manifest.d5297cb138b0df5fb625ff1bee336b3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adc54c8f9c4a4feebd233e309cb185a",
+    "url": "/facedetect/index.html"
+  },
+  {
+    "revision": "209136c065b02ab4acc1",
+    "url": "/facedetect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "82cebbc7e3b36007a6ec",
+    "url": "/facedetect/static/css/main.f8e422bc.chunk.css"
+  },
+  {
+    "revision": "209136c065b02ab4acc1",
+    "url": "/facedetect/static/js/2.d8b764b9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetect/static/js/2.d8b764b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82cebbc7e3b36007a6ec",
+    "url": "/facedetect/static/js/main.ecf5c085.chunk.js"
+  },
+  {
+    "revision": "a7a018dc83120ce99179",
+    "url": "/facedetect/static/js/runtime-main.837aade3.js"
+  },
+  {
+    "revision": "014560c03cbcab7f1adee1b2824cc5d6",
+    "url": "/facedetect/static/media/Brain.014560c0.svg"
+  }
+]);
